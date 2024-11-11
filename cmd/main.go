b77df529b02fe36f640174305915a5653fd82ba2
rodrigo462003 <rodrigo46.2003@gmail.com>
2024-11-11 11:14:32 +0000

    Reorganized main.go with router and handlers and added title and favicon.ico

--- cmd/main.go
+++ cmd/main.go
@@ -7,14 +7,22 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+func indexGetHandler(c echo.Context) error{
+	return c.Render(200, "index","hello")
+}
+
+func setupRoutes(e *echo.Echo){
+	e.File("/favicon.ico","images/favicon.ico")
+	e.Static("/css", "css")
+	e.GET("/", indexGetHandler)
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Renderer = templates.NewTemplate()
 
-	e.GET("/", func (c echo.Context) error {
-		return c.Render(200, "index","hello")
-	})
+	setupRoutes(e)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
